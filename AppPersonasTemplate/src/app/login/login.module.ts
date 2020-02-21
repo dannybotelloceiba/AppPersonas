d7c19debb778d31dbc8b7cde500462ab2d904e35
CEIBA\danny.botello <danny.botello@ceiba.com.co>
2020-02-21 09:56:41 -0500

    GRAL: Creación de componente de login

--- AppPersonasTemplate/src/app/login/login.module.ts
+++ AppPersonasTemplate/src/app/login/login.module.ts
@@ -1,5 +1,5 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-
+import { NativeScriptUIDataFormModule } from "nativescript-ui-dataform/angular";
 import { LoginRoutingModule } from './login-routing.module';
 import { NativeScriptCommonModule } from 'nativescript-angular/common';
 import { LoginComponent } from './login/login.component';
@@ -9,7 +9,7 @@ import { LoginComponent } from './login/login.component';
   declarations: [LoginComponent],
   imports: [
     LoginRoutingModule,
-    NativeScriptCommonModule
+    NativeScriptCommonModule,NativeScriptUIDataFormModule
   ],
   schemas: [NO_ERRORS_SCHEMA]
 })
--- AppPersonasTemplate/src/app/login/model/usuario.ts
+++ AppPersonasTemplate/src/app/login/model/usuario.ts
@@ -3,4 +3,10 @@
 export class Usuario {
     usuario: string;
     clave: string;
+
+
+    constructor(usuario: string, clave: string) {
+        this.usuario = usuario;
+        this.clave = clave;
+    }
 }
